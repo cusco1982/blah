559eda36696940a6a5208c82dc8651cf47d488fe
Julian <cusco2891@gmail.com>
2019-11-18 20:49:52 -0500

    contact func clear

--- client/src/pages/Contacts.js
+++ client/src/pages/Contacts.js
@@ -1,28 +1,28 @@
 import React, { Component } from "react";
 import Contact from "../components/Contact";
-import API from "../utils/API"
+// import API from "../utils/API"
 
 
 class Contacts extends Component {
 
-  state = {
-    fname: '',
-    lname: '',
-    email: '',
-    phone: '',
-    message: ''
-  }
+  // state = {
+  //   fname: '',
+  //   lname: '',
+  //   email: '',
+  //   phone: '',
+  //   message: ''
+  // }
 
-  onSubmit = () => {
-    const fname = this.state.fname;
-    const lname = this.state.lname;
-    const email = this.state.email;
-    const phone = this.state.phone;
-    const message = this.state.message;
+  // onSubmit = () => {
+  //   const fname = this.state.fname;
+  //   const lname = this.state.lname;
+  //   const email = this.state.email;
+  //   const phone = this.state.phone;
+  //   const message = this.state.message;
 
 
-    API.contact({ fname: fname, lname: lname, email: email, phone: phone, message: message })
-  }
+    // API.contact({ fname: fname, lname: lname, email: email, phone: phone, message: message })
+  // }
 
   render() {
     return (
